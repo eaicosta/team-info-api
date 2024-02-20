97b5c6aecc56d35fad71d7d7a2b227025535ea72
Davi Costa <davicostadelira04@gmail.com>
2024-02-20 15:00:20 -0300

    Project created

---
+++ src/main/java/com/devdavicosta/teaminfoapi/TeaminfoapiApplication.java
@@ -0,0 +1,13 @@
+package com.devdavicosta.teaminfoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeaminfoapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeaminfoapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devdavicosta/teaminfoapi/TeaminfoapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devdavicosta.teaminfoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeaminfoapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
