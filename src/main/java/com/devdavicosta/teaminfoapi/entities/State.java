4a5065b22cfd7597241488064fb1705a5c642381
Davi Costa <davicostadelira04@gmail.com>
2024-02-21 13:37:14 -0300

    Adding H2 Database for tests

--- src/main/java/com/devdavicosta/teaminfoapi/entities/State.java
+++ src/main/java/com/devdavicosta/teaminfoapi/entities/State.java
@@ -3,10 +3,20 @@ package com.devdavicosta.teaminfoapi.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="estados")
 public class State implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String uf;
